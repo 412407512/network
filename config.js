6533c277c92cc139cfb777160734b23645f19b6e
412407512 <soso.412407512@gmail.com>
2026-02-15 00:01:32 +0200

    comment

---
+++ config.js
@@ -0,0 +1,79 @@
+const GhaithConfig = {
+    "slider": [
+        {
+            "title": "أهلاً بكم في شبكة على كيفك لخدمات الانترنت",
+            "desc": "سرعة، ثبات، وسهولة في الاتصال"
+        },
+        {
+            "title": "دائماً على كيفك",
+            "desc": "اختر باقتك المفضلة واستمتع بالتصفح"
+        }
+    ],
+    "plans": [
+        {
+            "name": "باقة يومية 24 ساعة سرعة 2 ميجا",
+            "info": "سرعة جيدة",
+            "price": "3 شيكل"
+        },
+        {
+            "name": "باقة 10 ساعات سرعة 3 ميجا",
+            "info": "سرعة عالية ",
+            "price": "2 شيكل"
+        },
+        {
+            "name": "باقة 2 ساعة سرعة 5 ميجا ",
+            "info": "سرعة عالية جداً - لتحديث الهواتف والألعاب",
+            "price": "2 شيكل"
+        }
+    ],
+    "resellers": [
+        {
+            "name": "كفي فيريرو",
+            "address": "بجانب مستشفى الكويتي",
+            "phone": ""
+        }
+    ],
+    "support": {
+        "phone": "0595600454",
+        "address": "العنوان الرئيسي",
+        "whatsapp": "https:\/\/wa.me\/message\/3K7P7ZAN2FMFO1",
+        "onlineStore": "https://aqsa.convertcards.site"
+    },
+    "theme": "amber",
+    "areas": [
+        {
+            "area_name": "جامعة الأقصى",
+            "points": [
+                {
+                    "name": "1",
+                    "address": "1",
+                    "phone": ""
+                },
+                {
+                    "name": "2",
+                    "address": "2",
+                    "phone": ""
+                }
+            ]
+        },
+        {
+            "area_name": "مخيم السلام",
+            "points": [
+                {
+                    "name": "1",
+                    "address": "1",
+                    "phone": ""
+                },
+                {
+                    "name": "2",
+                    "address": "2",
+                    "phone": ""
+                }
+            ]
+        },
+        {
+            "area_name": "مخيم الشافعي",
+            "points": []
+        }
+    ]
+};
